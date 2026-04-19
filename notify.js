4e1dcab8e75b15c63fc8476a22438893785f7a2f
sumitrawat4321 <sumit.rawat4321@gmail.com>
2026-04-19 21:39:09 +0530

    first commit

---
+++ notify.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const client = require('twilio')(
+    process.env.TWILIO_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+
+  try {
+    await client.messages.create({
+      from: 'whatsapp:+14155238886',
+      to: 'whatsapp:+919997289858',
+      body: '🚀 QR scanned (POC demo)'
+    });
+
+    res.status(200).send("ok");
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+}
